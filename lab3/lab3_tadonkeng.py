e934852ea52c86e247964ddd7b560f7cc0a7131e
Arniden <atadonkeng@gmail.com>
2025-09-09 22:52:33 -0400

    complete lab3

--- lab3/lab3_tadonkeng.py
+++ lab3/lab3_tadonkeng.py
@@ -4,21 +4,37 @@ Lab 3, Python conditional Statement and loops
 Sep 8, 2025
 
 """
+
+"""
+Modify example 1
+- use while loop to validate if the user_number is between 1 and 9
+- user can only guess three times. This can be done using a for loop or a while
+"""
 #Conditional Statement
 print("\n ----- Example 1: if, elif,.... else -----")
-# guess a number between 1 and 9
-GUESS_NUM = 8
-#collect the number
-user_number = int(input("Guess a number: "))
-# compare
-if(user_number == GUESS_NUM):
-    print(f"Great Job! {user_number} is the guess number.")
-elif(user_number > GUESS_NUM):
-    print(f"{user_number} should be lower")
-elif(user_number < GUESS_NUM):
-    print(f"{user_number} should be higher")
+
+# Validate user_number between 1 and 9
+while True:
+    try:
+        user_number = int(input("Enter a number between 1 and 9: "))
+        if 1 <= user_number <= 9:
+            break
+        else:
+            print("Number must be between 1 and 9.")
+    except ValueError:
+        print("Invalid input. Please enter an integer.")
+
+# Guessing loop (3 attempts)
+target_number = user_number  # The number to guess
+for attempt in range(1, 4):
+    guess = int(input(f"Attempt {attempt}: Guess the number: "))
+    if guess == target_number:
+        print("Congratulations! You guessed the number.")
+        break
+    else:
+        print("Incorrect guess.")
 else:
-    print(f"ERROR!")
+    print(f"Sorry, you've used all attempts. The number was {target_number}.")
     
 print("End of guessing!")
 
@@ -49,5 +65,27 @@ print(f"At age {age_student}, the food is {lunch}")
 
 print("\n ----- Example 3: for loop as a counter-----")
 # 'for' loop enables the program to execute a code block multiple times.
-for n in range(3):
+for n in range(2,10):
     print(n)
+    
+print("\n ----- Example 4: for loop in a list -----")
+years = [2011, 2005, 1998, 1980, 1973]
+for y in years:
+    print(y)
+    
+for index in range(len(years)):
+    print(f"Year {index+1} = {years[index]}")
+    
+print("\n ----- Example 5: while loop as a counter -----")
+count = 1
+while count<=5:
+    print
+    count += 1
+    
+print("\n ----- Example 6: while loop to validate a number -----")
+#validate if a number is between -5 and 5 (inclusive)
+num = int(input("Enter a number between -5 and 5: "))
+while num<-5 or num>5:
+    num = int(input("ERROR! Enter a number between -5 and 5: "))
+    
+print(f"Entered number = {num} ")
